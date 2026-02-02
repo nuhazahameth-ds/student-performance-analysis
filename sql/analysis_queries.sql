f4a954b5b7ff6613c3745328d63667437da0d33e
Nuhaz Ahameth MR <nuhazahameth@gmail.com>
2026-02-02 20:34:43 +0530

    Add student_performance table creation script

--- sql/analysis_queries.sql
+++ sql/analysis_queries.sql
@@ -1,1 +1,10 @@
--- SQL file for Student Academic Performance Analysis
+-- Student Academic Performance Analysis
+-- Author: Nuhaz
+-- Description: Table creation for analyzing semester-wise performance
+
+CREATE TABLE student_performance (
+    semester NUMBER,
+    subject VARCHAR2(100),
+    credits NUMBER,
+    ca_percentage NUMBER
+);
